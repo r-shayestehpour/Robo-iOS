c7089217356f68c99ee146b98236232f3ae33974
Reza Shayestehpour <reza@gandom.co>
2017-05-20 12:20:41 +0430

    added the socket libs

--- robo/robo/ViewController.swift
+++ robo/robo/ViewController.swift
@@ -7,12 +7,40 @@
 //
 
 import UIKit
+import CoreMotion
 
 class ViewController: UIViewController {
+    
+    var motionManager : CMMotionManager = CMMotionManager()
 
+    @IBOutlet weak var slider: UISlider!
+    @IBOutlet weak var webView: UIWebView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.slider.maximumValue = 100
+        self.slider.minimumValue = -100
+        self.slider.value = 0
+        
+        self.initMotionManager()
+        
+        let url = URL(string: "https://www.google.com")
+        let requestObject = URLRequest(url: url!)
+        self.webView.loadRequest(requestObject)
+        self.webView.scrollView.isScrollEnabled = false
+        self.webView.scrollView.bounces = false
+    }
+    
+    func initMotionManager() {
+        if self.motionManager.isDeviceMotionAvailable {
+            self.motionManager.deviceMotionUpdateInterval = 0.01
+            self.motionManager.startDeviceMotionUpdates(to: OperationQueue.main, withHandler: { data, error in
+                print(data?.gravity.x ?? "XXX", data?.gravity.y ?? "YYY", data?.gravity.z ?? "ZZZ")
+                
+                self.slider.setValue(Float((data?.gravity.y)! * (-100.0)), animated: true)
+            })
+        }
     }
 
     override func didReceiveMemoryWarning() {
